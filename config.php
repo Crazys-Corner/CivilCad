8f79a066f347713eef1a08a8bb1b1e9f85f876e4
PARKER SAWH <pdxvbucks@gmail.com>
2023-08-06 13:44:24 -0600

    My commit test

--- config.php
+++ config.php
@@ -9,4 +9,4 @@ const DefaultPassword = 'Name';
 const ProductKey = 'A1B2C3';
 const ServerIpAddress = '192.168.1.1';
 const ServerGame = 'Minecraft';
-const DiscordLink = 'aaaaa';
+const DiscordLink = 'aabaa';
