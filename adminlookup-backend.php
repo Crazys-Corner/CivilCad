8d6ed331ff15d1ab947d6b73b874fddb12b03b0d
Crazys-Corner <craazygaming1021@gmail.com>
2023-08-08 16:44:44 -0400

    Admin Lookup Scripts

--- adminlookup-backend.php
+++ adminlookup-backend.php
@@ -1,1 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Information</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+        }
+        h2 {
+            margin-bottom: 10px;
+        }
+        .user-info {
+            margin-bottom: 20px;
+        }
+        .transaction {
+            margin-bottom: 5px;
+        }
+        .no-data {
+            font-style: italic;
+            color: #999;
+        }
+        .container {
+            margin-top: 10vh;
+        }
+    </style>
+</head>
+<body>
 
+    <h1>User Information</h1>
+    <div style="margin-top: 5vh;">
+    <h3>Submitted Data: <?php     require("verify.php");
+    $input = $_POST['id']; echo $input ?> </h3>
+    </div>
+ <div class="container">    
+    <?php
+
+
+    $connection = mysqli_connect(DatabaseAddress, DatabaseUsername, DatabasePassword, DatabaseName);
+
+    if (!$connection) {
+        die("Connection Failed: " . mysqli_connect_error());
+    }
+
+    // Perform the queries
+
+    $query1 = "SELECT * FROM users WHERE id64 = '$input'";
+    $query2 = "SELECT * FROM CB WHERE owner = '$input'";
+    $query3 = "SELECT * FROM transactions WHERE user_id = '$input'";
+
+    $result1 = mysqli_query($connection, $query1);
+    $result2 = mysqli_query($connection, $query2);
+    $result3 = mysqli_query($connection, $query3);
+
+    if (!$result1 || !$result2 || !$result3) { // Fixed the condition here
+        die("Query error: " . mysqli_error($connection));
+    }
+    ?>
+
+    <div class="user-info">
+        <h2>User Account:</h2>
+        <?php
+        if (mysqli_num_rows($result1) > 0) {
+            while ($row = mysqli_fetch_assoc($result1)) {
+                echo "ID: " . $row['id'] . "<br>";
+                echo "Username: " . $row['username'] . "<br>";
+                echo "Email: " . $row['email'] . "<br>";
+            }
+        } else {
+            echo "<p class='no-data'>No User Account Found</p>";
+        }
+        ?>
+    </div>
+    
+    <div class="user-info">
+        <h2>Banking Details</h2>
+        <?php
+        if (mysqli_num_rows($result2) > 0) {
+            while ($row = mysqli_fetch_assoc($result2)) {
+                echo "Personal Account Number: " . $row['personalAccountNumber'] . " Balance: " . $row['balance'] . "<br>"; 
+            }
+        } else {
+            echo "<p class='no-data'>No Banking Details Found</p>";
+        }
+        ?>
+    </div>
+    
+    <div class="transactions">
+        <h2>Banking Transactions</h2>
+        <?php
+        if (mysqli_num_rows($result3) > 0) {
+            while ($row = mysqli_fetch_assoc($result3)) {
+                echo "<p class='transaction'>Transaction: " . $row['description'] . "</p>";
+            }
+        } else {
+            echo "<p class='no-data'>No Transactions Recorded</p>";
+        }
+        ?>
+    </div>
+
+    <?php
+    mysqli_close($connection);
+    ?>
+    </div> 
+</body>
+</html>
--- adminlookup.php
+++ adminlookup.php
@@ -1,1 +1,77 @@
+<?php 
+ require('verify.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Lookup</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+        }
 
+        .container {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            width: 300px;
+        }
+
+        h1 {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        form {
+            display: flex;
+            flex-direction: column;
+        }
+
+        label {
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+
+        input[type="text"] {
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 3px;
+            margin-bottom: 15px;
+        }
+
+        button[type="submit"] {
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            padding: 10px 15px;
+            border-radius: 3px;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+        }
+
+        button[type="submit"]:hover {
+            background-color: #0056b3;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Admin Lookup</h1>
+        <form action="adminlookup-backend.php" method="post">
+            <label for="id">User Steam64ID:</label>
+            <input type="text" name="id">
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+</body>
+</html>
