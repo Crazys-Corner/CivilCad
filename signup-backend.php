5fc0965a2499a5c620dd932e738dec4e834a6a3a
rooveinnit <pdxvbucks@gmail.com>
2023-08-07 19:56:34 -0600

    holy shit

--- signup-backend.php
+++ signup-backend.php
@@ -1,18 +1,25 @@
-<?php
-// signup shit
-require ("verify.php");
-
-
-// check if user is logged in
-// Function to check if the user is already logged in
-function isUserLoggedIn() {
-  return isset($_SESSION['user_id']);
-}
+<php
 function signupUser($username, $password, $email) {
-  $conn = new mysqli(DatabaseAddress, DatabaseUsername, DatabasePassword, DatabaseName);
-}
-// Sanitize input to prevent SQL injection
+    // Connect to the database
+    $conn = new mysqli(DatabaseAddress, DatabaseUsername, DatabasePassword, DatabaseName);
 
-$username = $conn->real_escape_string($username);
-$password = $conn->real_escape_string($password);
-$email = $conn->real_escape_string($password);
+    // Sanitize input to prevent SQL injection
+    $username = $conn->real_escape_string($username);
+    $password = $conn->real_escape_string($password);
+    $email = $conn->real_escape_string($email);
+
+    // Hash the password for security
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    // Insert the user into the database
+    $stmt = $conn->prepare("INSERT INTO users (username, password, email) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $username, $hashed_password, $email);
+    if ($stmt->execute()) {
+        // User successfully signed up
+        return true;
+    } else {
+        // Error occurred while signing up user
+        return false;
+    }
+}
+<?
